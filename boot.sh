41740e73f3b565d80cf131a5f95b7fe99e21fa5f
student <student>
2025-03-10 00:07:31 +0100

    dockerize

--- boot.sh
+++ boot.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# this script is used to boot a Docker container
+# Initialize
+if [ ! -d "migrations" ]; then
+    echo "Initializing database migrations..."
+    flask db init
+fi
+# Migrate and upgrade the database if necessary
 while true; do
     flask db upgrade
     if [[ "$?" == "0" ]]; then
         break
     fi
-    echo Deploy command failed, retrying in 5 secs...
+    echo "Deploy command failed, retrying in 5 secs..."
     sleep 5
 done
-exec gunicorn -b :5000 --access-logfile - --error-logfile - dbwe-app:app
+
+# Starte Gunicorn als WSGI-Server
+exec gunicorn -w 2 -b :5000 --access-logfile - --error-logfile - dbwe-app:app
