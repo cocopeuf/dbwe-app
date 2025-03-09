7a56f612e693a6254415832d7b3304fd89fd780d
student <student>
2025-03-09 23:41:47 +0100

    fix edit page

--- app/main/routes.py
+++ app/main/routes.py
@@ -534,3 +534,18 @@ def delete_message(message_id):
     db.session.commit()
     flash(_('Message deleted successfully.'))
     return redirect(url_for('main.messages'))
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(original_username=current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title=_('Edit Profile'), form=form)
